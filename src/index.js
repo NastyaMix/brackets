240b621fb4fb4921519ad7b435d53c69a2aa21fe
NastyaMix <kravchenkonastik@gmail.com>
2023-02-02 00:01:49 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let open = [];
+
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (open[open.length - 1] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1]) {
+        open.pop();
+      } else {
+        if (str[i] == bracketsConfig[j][0]) {
+          open.push(str[i]);
+        }
+      }
+    }
+  }
+  return (open.length == 0) ? true : false;
 }
+
